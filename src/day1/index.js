4713831ac17c0dff091d6a602ca8898fc7f935a9
Marco Sarrió Ferrández <marcovillena24@gmail.com>
2022-12-02 16:03:05 +0200

    Day 1 & 2

---
+++ src/day1/index.js
@@ -0,0 +1,14 @@
+import { input } from "./input.js";
+
+const elfs = input.split('\n\n').map(elf => elf.split('\n'))
+
+export const partOne = () => {
+    const elfsKcal = elfs.map(elf => elf.reduce((sum, num) => sum + Number(num), 0))
+    return Math.max(...elfsKcal)
+}
+
+export const partTwo = () => {
+    const elfsKcal = elfs.map(elf => elf.reduce((sum, num) => sum + Number(num), 0))
+    const elfsRank = elfsKcal.slice().sort((a, b) => b - a)
+    return elfsRank[0] + elfsRank[1] + elfsRank[2]
+}
