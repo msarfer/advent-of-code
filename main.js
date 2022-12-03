de2840b95dd534c21437b468b6f66346e5f45bda
Marco Sarrió Ferrández <marcovillena24@gmail.com>
2022-12-03 12:36:33 +0200

    Day 3

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 import * as day1 from "./src/day1/index.js";
 import * as day2 from "./src/day2/index.js";
+import * as day3 from './src/day3/index.js'
+
 const challenges = [
     {id: 1.1, func: day1.partOne()},
     {id: 1.2, func: day1.partTwo()},
     {id: 2.1, func: day2.partOne()},
-    {id: 2.2, func: day2.partTwo()}
+    {id: 2.2, func: day2.partTwo()},
+    {id: 3.1, func: day3.partOne()},
+    {id: 3.2, func: day3.partTwo()},
 ]
 
 const display = ({id, func}) => {
---
+++ src/day3/index.js
@@ -0,0 +1,35 @@
+import { input } from "./input.js";
+
+const getPriority = (char) => {
+    const asciiCode = char.charCodeAt(0)
+    return asciiCode > 96 ? asciiCode - 96 : asciiCode - 38
+}
+
+export const partOne = () => {
+    const rucksacks = input.split('\n').map(rucksack => {
+        rucksack = rucksack.split('')
+        return [rucksack.slice(0, rucksack.length / 2), rucksack.slice(rucksack.length / 2)]
+    })
+
+
+    const intersection = rucksacks.map(([first, second]) => {
+        return first.filter((item) => second.includes(item))
+    })
+
+    return intersection.reduce((sum, type) => sum + getPriority(...type), 0)
+
+}
+
+export const partTwo = () => {
+    const rucksacks = input.split('\n')
+    const groups = []
+    for (let i = 0; i < rucksacks.length; i += 3) {
+        groups.push([rucksacks[i].split(''), rucksacks[i + 1].split(''), rucksacks[i + 2].split('')])
+    }
+
+    const intersection = groups.map(group => {
+        return group.reduce((a, b) => a.filter(c => b.includes(c)))
+    })
+
+    return intersection.reduce((sum, type) => sum + getPriority(...type), 0)
+}
