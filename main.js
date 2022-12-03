0d03539eb7eba5c1f7c28b00113819700c2a4e9c
Marco Sarrió Ferrández <marcovillena24@gmail.com>
2022-12-03 16:25:49 +0200

    Add linter

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-import * as day1 from "./src/day1/index.js";
-import * as day2 from "./src/day2/index.js";
+import * as day1 from './src/day1/index.js'
+import * as day2 from './src/day2/index.js'
 import * as day3 from './src/day3/index.js'
 
 const challenges = [
-    {id: 1.1, func: day1.partOne()},
-    {id: 1.2, func: day1.partTwo()},
-    {id: 2.1, func: day2.partOne()},
-    {id: 2.2, func: day2.partTwo()},
-    {id: 3.1, func: day3.partOne()},
-    {id: 3.2, func: day3.partTwo()},
+  {id: 1.1, func: day1.partOne()},
+  {id: 1.2, func: day1.partTwo()},
+  {id: 2.1, func: day2.partOne()},
+  {id: 2.2, func: day2.partTwo()},
+  {id: 3.1, func: day3.partOne()},
+  {id: 3.2, func: day3.partTwo()},
 ]
 
 const display = ({id, func}) => {
-    console.log(`> Challenge #${id}`)
-    console.log(`> ${func}\n`)
+  console.log(`> Challenge #${id}`)
+  console.log(`> ${func}\n`)
 }
 console.log('> Advent of Code 2022\n')
 challenges.map(challenge => display(challenge))
--- src/day2/index.js
+++ src/day2/index.js
@@ -1,64 +1,64 @@
-import { input } from "./input.js";
+import { input } from './input.js'
 
 const plays = input.split('\n').map(play => play.split(' '))
 
 const points = {
-    'X': 1,
-    'Y': 2,
-    'Z': 3
+  'X': 1,
+  'Y': 2,
+  'Z': 3
 }
 
 export const partOne = () => {
-    const rules = {
-        'X': {
-            'A': 3 + points['X'],
-            'B': points['X'],
-            'C': 6 + points['X'],
-        },
-        'Y': {
-            'A': 6 + points['Y'],
-            'B': 3 + points['Y'],
-            'C': points['Y'],
-        },
-        'Z': {
-            'A': points['Z'],
-            'B': 6 + points['Z'],
-            'C': 3 + points['Z'],
-        }
+  const rules = {
+    'X': {
+      'A': 3 + points['X'],
+      'B': points['X'],
+      'C': 6 + points['X'],
+    },
+    'Y': {
+      'A': 6 + points['Y'],
+      'B': 3 + points['Y'],
+      'C': points['Y'],
+    },
+    'Z': {
+      'A': points['Z'],
+      'B': 6 + points['Z'],
+      'C': 3 + points['Z'],
     }
+  }
 
-    let score = 0
-    plays.map(([elf, me]) => {
-        score += rules[me][elf]
-    })
+  let score = 0
+  plays.map(([elf, me]) => {
+    score += rules[me][elf]
+  })
 
-    return score
+  return score
 }
 
 export const partTwo = () => {
-    const rules = {
-        'A': {
-            'X': points['Z'],
-            'Y': 3 + points['X'],
-            'Z': 6 + points['Y'],
-        },
-        'B': {
-            'X': points['X'],
-            'Y': 3 + points['Y'],
-            'Z': 6 + points['Z'],
-        },
-        'C': {
-            'X': points['Y'],
-            'Y': 3 + points['Z'],
-            'Z': 6 + points['X'],
-        }
+  const rules = {
+    'A': {
+      'X': points['Z'],
+      'Y': 3 + points['X'],
+      'Z': 6 + points['Y'],
+    },
+    'B': {
+      'X': points['X'],
+      'Y': 3 + points['Y'],
+      'Z': 6 + points['Z'],
+    },
+    'C': {
+      'X': points['Y'],
+      'Y': 3 + points['Z'],
+      'Z': 6 + points['X'],
     }
+  }
 
-    let score = 0
-    plays.map(([elf, play]) => {
-        score += rules[elf][play]
-    })
+  let score = 0
+  plays.map(([elf, play]) => {
+    score += rules[elf][play]
+  })
 
-    return score
+  return score
 
 }
